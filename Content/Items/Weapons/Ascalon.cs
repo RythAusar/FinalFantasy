abe2f562c3a9ae8dd8ca3fdda0b266e81b51171b
Gestalht <>
2025-03-05 22:15:24 -0500

    Changed Ascalon

--- Content/Items/Weapons/Ascalon.cs
+++ Content/Items/Weapons/Ascalon.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.Xna.Framework;
+﻿using Luminance.Core.Graphics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using Stubble.Core.Settings;
 using Terraria;
+using Terraria.DataStructures;
 using Terraria.ID;
 using Terraria.ModLoader;
 
@@ -16,14 +19,19 @@ namespace FinalFantasy.Content.Items.Weapons
 
         public override void SetDefaults()
         {
-            Item.width = 20;
-            Item.height = 20;
-            Item.noMelee = false;
-            Item.shoot = ProjectileID.Arkhalis;
+            Item.width = 120;
+            Item.height = 120;
+            Item.shoot = ProjectileID.StarWrath;
+            Item.shootSpeed = 16f;
             Item.useStyle = ItemUseStyleID.Swing;
             Item.UseSound = SoundID.Cockatiel;
-            Item.shootSpeed = 16f;
             Item.damage = 1488;
+            Item.useTime = 10;
+            Item.useAnimation = 10;
+            Item.autoReuse = true;
+            Item.crit = 50;
+            Item.knockBack = 10;
+            Item.DamageType = DamageClass.Generic;
 
         }
 
@@ -31,6 +39,85 @@ namespace FinalFantasy.Content.Items.Weapons
         {
             return true;
         }
+
+        public override void HoldItem(Player player)
+        {
+            if (Main.myPlayer == player.whoAmI)
+            {
+                if(Main.MouseWorld.X > player.Center.X)
+                {
+                    player.direction = 1;
+                }else if(Main.MouseWorld.X < player.Center.X)
+                {
+                    player.direction = -1;
+                }
+            }
+        }
+
+
+        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+        {
+            if(player.altFunctionUse == 2)
+            {
+                Vector2 target = Main.screenPosition + new Vector2(Main.mouseX, Main.mouseY);
+                float ceilingLimit = target.Y;
+                if(ceilingLimit > player.Center.Y - 200f)
+                {
+                    ceilingLimit = player.Center.Y - 200f;
+                }
+                for (int i = 0; i < 3; i++)
+                {
+                    position = player.Center - new Vector2(Main.rand.NextFloat(401) * player.direction, 600f);
+                    position.Y -= 100 * i;
+                    Vector2 heading = target - position;
+
+                    if (heading.Y < 0f)
+                    {
+                        heading.Y *= -1f;
+                    }
+
+                    if (heading.Y < 20f)
+                    {
+                        heading.Y = 20f;
+                    }
+
+                    heading.Normalize();
+                    heading *= velocity.Length();
+                    heading.Y += Main.rand.Next(-40, 41) * 0.02f;
+                    Projectile.NewProjectile(source, position, heading, type, damage * 2, knockback, player.whoAmI, 0f, ceilingLimit);
+                }
+            }
+            return false;
+        }
+        
+
+
+        public override bool PreDrawTooltipLine(DrawableTooltipLine line, ref int yOffset)
+        {
+            if (line.Mod == "Terraria" && line.Name == "ItemName")
+            {
+                Main.spriteBatch.End();//ending drawing commands
+                Main.spriteBatch.Begin(SpriteSortMode.Immediate, null, null, null, null, null, Main.UIScaleMatrix);//starts sprite batch using Immediate mode. Immediate mode, each draw call is processed as soon as it's made, which is necessary when you want to change rendering states (like applying a shader).
+                ManagedShader shader = ShaderManager.GetShader("FinalFantasy.Text");//No need to load the shader through Load(). Luminance.dll does it for me. Just FinalFantasy.(shader file name)
+                shader.TrySetParameter("mainColor", new Color(252, 0, 50));//setting main color
+                shader.TrySetParameter("secondaryColor", new Color(0, 255, 200));//setting secondary color
+                shader.Apply("ModifiedShader");//passing a pass. This time it is PulseUpwards
+                Utils.DrawBorderString(Main.spriteBatch, line.Text, new Vector2(line.X, line.Y), Color.White, 1);//draw tooltip
+                Main.spriteBatch.End();
+                Main.spriteBatch.Begin(SpriteSortMode.Deferred, null, null, null, null, null, Main.UIScaleMatrix);
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe().AddIngredient(ItemID.DirtBlock, 1).AddTile(TileID.WorkBenches);
+        }
+
 
     }
 }
